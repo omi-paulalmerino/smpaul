f25954030a7c9cef911da7f472299c4562a676a9
Paul <jp.almerino305@gmail.com>
2024-09-14 20:17:04 +0800

    Fix attempt setuptools 1

--- setup.py
+++ setup.py
@@ -7,12 +7,17 @@ REQUIRES = [
   "requests",
 ]
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 
 setup(
     name=NAME,
     version=VERSION,    
     description=""" The description of the package   """,
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/omi-paulalmerino/smpaul",
     author="Paul Almerino",
