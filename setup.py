28d8f5734168d5e2ef8bc8e4fc18744751184592
Paul <jp.almerino305@gmail.com>
2024-09-15 07:49:16 +0800

    Change on setup and workflow

--- setup.py
+++ setup.py
@@ -30,13 +30,9 @@ setup(
     install_requires=REQUIRES,
     classifiers=[
         'Development Status :: 1 - Planning',
-        'Intended Audience :: Science/Research',
+        'Intended Audience :: SM Application Development',
         'License :: OSI Approved :: BSD License',  
-        'Operating System :: POSIX :: Linux',        
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Operating System :: POSIX :: Linux',    
+        'Programming Language :: Python :: 3.9',
     ],
 )
